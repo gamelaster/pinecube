d4eefa3f7a1cf0806a3e8d59ca8831e66a57f824
Marek Kraus <gamelaster@outlook.com>
2021-12-05 18:39:35 +0100

    Latest IDB + gap_to_field script

---
+++ scripts/gap_to_field.dart
@@ -0,0 +1,17 @@
+import 'package:dart_clipboard/dart_clipboard.dart';
+
+void main() {
+  String clipboard = Clipboard.getContents();
+  //String clipboard =
+  //    "  *(_DWORD *)&h264Context->gapC[468] = 18;\n*(_DWORD *)&h264Context->gapC[428] = 0x200000;\n*(_DWORD *)&h264Context->gapC[432] = 30000;\n*(_DWORD *)&h264Context->gapC[420] = 25;\n*(_DWORD *)&h264Context->gapC[448] = 45;";
+  clipboard = clipboard
+      .replaceAllMapped(RegExp(r'\-\>gap([A-F0-9]+)\[([0-9]+)\]'), (match) {
+    String address =
+        (int.parse(match.group(1), radix: 16) + int.parse(match.group(2)))
+            .toRadixString(16)
+            .toUpperCase();
+    return "->field_$address";
+  });
+  print("Converted :-)");
+  Clipboard.setContents(clipboard);
+}
